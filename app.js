2cd12bec5f72d73153ff43156917ff8417c0a5fa
Raghupathi <raghupathi.jan.01@gmail.com>
2023-06-23 10:24:40 +0530

    not found error

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
+const mainRoute = require('./controller/vidjot')
 
 const app = express()
+
 // console.log(exphbs) // contains ExpressHandlebars, create, and engine
 
 // // Json parser
 // app.use(express.json())
 
 // Body parser Middleware
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //Load Idea model
@@ -27,51 +30,54 @@ app.set('view engine', 'handlebars')
 
 // How middleware works
 // app.use(function(req,res,next){
-//     req.name = 'Raghupathi'
-//     console.log(Date.now())
-//     next()
-// })
-
-app.get('/', (req, res) => {
-    const title = 'Welcome'
-    // console.log(req.name)
+    //     req.name = 'Raghupathi'
+    //     console.log(Date.now())
+    //     next()
+    // })
+    
+    app.get('/', (req, res) => {
+        const title = 'Welcome'
+        // console.log(req.name)
     // res.send(`Hi, Welcome ${req.name}`)
     res.render('index', {
         title: title
     })
-
+    
 })
 
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     res.render('about')
 })
 
 // Add Idea Form
-app.get('/ideas/add', (req,res) => {
-     res.render('ideas/add')
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add')
 })
 
 // Process Form
-app.post('/ideas',(req, res) => {
+app.post('/ideas', (req, res) => {
     let errors = [];
-
-    if(!req.body.title){
-        errors.push({text: 'Please add a title'})
+    
+    if (!req.body.title) {
+        errors.push({ text: 'Please add a title' })
     }
-    if(!req.body.details){
-        errors.push({text:'Please add some details'})
+    if (!req.body.details) {
+        errors.push({ text: 'Please add some details' })
     }
-
-    if(errors.length > 0){
+    
+    if (errors.length > 0) {
         res.render('ideas/add', {
             errors: errors,
             title: req.body.title,
             details: req.body.details
         })
     } else {
-        new Idea(req.body )
+        new Idea(req.body)
     }
 })
+app.use('/vidjot', mainRoute)
+app.use(notFound)
+
 const port = process.env.POR || 5000
 
 const start = async () => {
@@ -83,7 +89,7 @@ const start = async () => {
     } catch (error) {
         console.log(error)
     }
-
+    
 }
 
 start()
---
+++ controller/vidjot.js
@@ -0,0 +1,6 @@
+const index =  async (req,res) => {
+    const title = 'Welcome'
+    res.send("HI")
+}
+
+module.exports = index
